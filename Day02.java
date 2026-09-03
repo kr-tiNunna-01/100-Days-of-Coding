efb20c3b35fed9aba425ba135d20e652bd71f86f
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-03 22:43:10 +0800

    Change favorite flower from 'bunga telang' to 'bunga matahari'

---
+++ Day02.java
@@ -0,0 +1,23 @@
+public class Day02 {
+    public static void main(String[] args) {
+
+        // println()
+        System.out.println("=== PROFIL MAHASISWA ===");
+        System.out.println("Nama : Unna");
+        System.out.println("Prodi : Informatika");
+        System.out.println("Hobi : Berkebun, mendengarkan musik, bernyanyi.");
+
+        // print()
+        System.out.print("Tanaman favorit saya adalah ");
+        System.out.print("bunga matahari.\n");
+
+        // printf()
+        String musik = "Melanie Martinez";
+        String warna = "Pink";
+
+        System.out.printf(
+            "Saya suka mendengarkan %s dan warna favorit saya adalah %s.",
+            musik, warna
+        );
+    }
+}
