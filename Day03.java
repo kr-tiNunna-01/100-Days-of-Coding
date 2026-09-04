b092279cb221a5b34317643f3bde2441061740d0
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-04 19:53:10 +0800

    Create Day03.java

---
+++ Day03.java
@@ -0,0 +1,13 @@
+public class Day03 {
+    public static void main(String[] args) {
+
+        System.out.println("=== BIODATA ===\n" +
+                           "Nama\t: Unna\n" +
+                           "Jurusan\t: Informatika\n" +
+                           "Hobi\t: Gardening\n" +
+                           "Quote\t: \"Stay curious\"\n");
+
+        System.out.println("Contoh path file:");
+        System.out.println("C:\\Users\\Unna\\Documents");
+    }
+}
