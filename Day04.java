b473e665a6613dc845e49f912d3778796d73f0d3
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-05 21:42:24 +0800

    Add Day04 class with main method and variables

---
+++ Day04.java
@@ -0,0 +1,14 @@
+public class Day04 {
+    public static void main(String[] args) {
+
+        byte umur = 20;
+        short tahunLahir = 2006;
+        int jumlahMahasiswa = 1500;
+        long jumlahBintang = 922337203685477580L;
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Tahun lahir: " + tahunLahir);
+        System.out.println("Jumlah mahasiswa: " + jumlahMahasiswa);
+        System.out.println("Jumlah bintang: " + jumlahBintang);
+    }
+}
