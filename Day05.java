ee9541b3d49b8b7ec91b03db2e7088c9f4aed424
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-06 19:41:15 +0800

    Add Day05 class with main method for float and double

---
+++ Day05.java
@@ -0,0 +1,10 @@
+public class Day05 {
+    public static void main(String[] args) {
+
+        float a = 12.5f;
+        double b = 25.75;
+
+        System.out.println("Nilai float : " + a);
+        System.out.println("Nilai double: " + b);
+    }
+}
