adceaf3a7cd10dfe12afa2d9d940fa28d048b71a
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-09 20:15:02 +0800

    Add Day08 class to demonstrate variable usage

---
+++ Day08.java
@@ -0,0 +1,14 @@
+public class Day08 {
+    public static void main(String[] args) {
+
+        int umur; // Deklarasi variabel
+
+        umur = 20; // Inisialisasi variabel
+
+        System.out.println("Umur saat ini: " + umur);
+
+        umur = 21; // Update nilai variabel
+
+        System.out.println("Umur tahun depan: " + umur);
+    }
+}
