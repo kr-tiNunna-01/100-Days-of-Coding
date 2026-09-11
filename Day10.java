2abdb91196b1b6f6cdce3e9257653b94463eae15
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-11 23:50:25 +0800

    Update Day10.java

--- Day10.java
+++ Day10.java
@@ -1,13 +1,29 @@
 public class Day10 {
     public static void main(String[] args) {
 
+        // Menampilkan batas minimum dan maksimum tipe data byte
         System.out.println("Byte : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+
+        // Menampilkan batas minimum dan maksimum tipe data short
         System.out.println("Short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+
+        // Menampilkan batas minimum dan maksimum tipe data int
         System.out.println("Integer : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+
+        // Menampilkan batas minimum dan maksimum tipe data long
         System.out.println("Long : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
 
+        // Menampilkan batas minimum dan maksimum tipe data float
         System.out.println("Float : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+
+        // Menampilkan batas minimum dan maksimum tipe data double
         System.out.println("Double : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+
+        // Menampilkan batas minimum dan maksimum tipe data char
+        System.out.println("Char : " + (int) Character.MIN_VALUE + " sampai " + (int) Character.MAX_VALUE);
+
+        // Menampilkan kemungkinan nilai yang dapat disimpan oleh boolean
+        System.out.println("Boolean : true atau false");
 
     }
 }
