716164b4dcedd0c3006cde69e407570361c6c546
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-12 20:42:34 +0800

    Create Day11Scanner.java

---
+++ Day11Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day11Scanner {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama bunga favoritmu: ");
+        String bunga = input.nextLine();
+
+        System.out.println("Bunga favoritmu adalah " + bunga);
+    }
+}
