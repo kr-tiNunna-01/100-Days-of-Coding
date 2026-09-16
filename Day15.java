092d132afbdf46ab4345eb02fdb14dbf223b8049
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-16 19:08:59 +0800

    Fix formatting in Day15 class declaration

---
+++ Day15.java
@@ -0,0 +1,19 @@
+public class Day15{
+    public static void main(String[] args) {
+
+        // Perkalian
+        int jumlahAlbumMelanie = 4;
+        int trackPerAlbum = 13;
+
+        int totalTrack = jumlahAlbumMelanie * trackPerAlbum;
+
+        System.out.println("=== Perkalian ===");
+        System.out.println("Total track Melanie Martinez = " + totalTrack);
+
+        // Pembagian
+        int rataRataTrack = totalTrack / jumlahAlbumMelanie;
+
+        System.out.println("\n=== Pembagian ===");
+        System.out.println("Rata-rata track per album = " + rataRataTrack);
+    }
+}
