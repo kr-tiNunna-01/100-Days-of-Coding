af759603bb2f18867b682db2478b0d43063bc9f1
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-16 18:58:13 +0800

    Add Day15Perkalian class to calculate total bibit

---
+++ Day15Perkalian.java
@@ -0,0 +1,11 @@
+public class Day15Perkalian {
+    public static void main(String[] args) {
+
+        int jumlahBibit = 12;
+        int jumlahPot = 4;
+
+        int totalBibit = jumlahBibit * jumlahPot;
+
+        System.out.println("Total bibit = " + totalBibit);
+    }
+}
