020f0d31e01460f1a202a613d5e0d3f0272816bf
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-17 21:44:03 +0800

    Update Day16.java

--- Day16.java
+++ Day16.java
@@ -1,13 +1,13 @@
-public class Day16 {
+// Operator Aritmatika Modulus atau Sisa Bagi (%)
+
+public class Day16{
     public static void main(String[] args) {
 
         int jumlahBungkusKopi = 25;
         int jumlahCangkir = 4;
 
-        int kopiPerCangkir = jumlahBungkusKopi / jumlahCangkir;
         int sisaBungkusKopi = jumlahBungkusKopi % jumlahCangkir;
 
-        System.out.println("Bungkus kopi per cangkir = " + kopiPerCangkir);
         System.out.println("Sisa bungkus kopi = " + sisaBungkusKopi);
     }
 }
