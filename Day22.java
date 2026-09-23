2a6b0a4a78a9296f15736ac55f5e18d38a45482e
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-23 23:07:16 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,21 @@
+public class Day22 {
+    public static void main(String[] args) {
+
+        // Nilai awal
+        int a = 100;
+        int b = 2000;
+
+        System.out.println("Sebelum ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Menukar nilai menggunakan variabel sementara
+        int oli = a;
+        a = b;
+        b = oli;
+
+        System.out.println("\nSetelah ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
