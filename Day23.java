0c26d8c0c6e708565b44041e99c1cec714fcfa70
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-24 23:24:47 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,14 @@
+public class Day23 {
+    public static void main(String[] args) {
+
+        // Menentukan sisi persegi
+        int sisi = 10;
+
+        // Menghitung luas persegi
+        int luas = sisi * sisi;
+
+        // Menampilkan hasil
+        System.out.println("Sisi persegi = " + sisi);
+        System.out.println("Luas persegi = " + luas);
+    }
+}
