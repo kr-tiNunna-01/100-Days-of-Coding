2556b67e6eda514d70b0d23af722ccd4e0161105
kr-tiNunna-01 <kristinonpc@gmail.com>
2026-09-24 23:27:01 +0800

    Update Day23.java

--- Day23.java
+++ Day23.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class Day23 {
     public static void main(String[] args) {
 
-        // Menentukan sisi persegi
-        int sisi = 10;
+        // Membuat Scanner untuk menerima input dari pengguna
+        Scanner input = new Scanner(System.in);
+
+        // Meminta pengguna memasukkan panjang sisi
+        System.out.print("Masukkan panjang sisi persegi: ");
+        int sisi = input.nextInt();
 
         // Menghitung luas persegi
         int luas = sisi * sisi;
 
         // Menampilkan hasil
-        System.out.println("Sisi persegi = " + sisi);
         System.out.println("Luas persegi = " + luas);
+
+        // Menutup Scanner
+        input.close();
     }
 }
